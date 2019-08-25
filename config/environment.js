10a7d467d1a46d10d21769d391d638f04b5edd43
Abbie Glunn <aglunn@gmail.com>
2019-08-24 19:12:28 -0700

    Deployment related updates

--- config/environment.js
+++ config/environment.js
@@ -3,9 +3,8 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'mountain-portfolio',
-    podModulePrefix: 'mountain-portfolio/pods',
     environment,
-    rootURL: '/',
+    rootURL: 'sparkles.cf/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
