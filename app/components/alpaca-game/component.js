55c9a2b9c04f75b782f98b9d8c8cb177707d9494
Abbie Glunn <aglunn@gmail.com>
2019-09-29 21:40:14 -0700

    Add sprite with animations

--- app/components/alpaca-game/component.js
+++ app/components/alpaca-game/component.js
@@ -1,39 +1,119 @@
 import Component from '@ember/component';
+import Spriteling from 'spriteling';
 
 export default Component.extend({
-  keysPressed: [false,false,false,false],
-  left: 50,
-  top: 0,
-  typedStrings: ["Ember apps.", "RESTful APIs.", "React components.", "SQL queries.", "automated tests with Selenium."],
+  alpacaSprite: {},
+  spriteScript: '',
+  facingLeft: false,
+  leftPosition: 50,
+  topPosition: 0,
 
-  didRender: function() {
+  didInsertElement() {
+    this._super(...arguments);
+
+    const sprite = new Spriteling({
+       url: '/img/alpaca_spritesheet.png',
+       cols: 12,
+       rows: 1,
+       startSprite: 12,
+       tempo: .5,
+     }, '#sprite');
+    sprite.addScript('runLeft', [
+       { sprite: 3 },
+       { sprite: 4 },
+       { sprite: 5 },
+       { sprite: 6 }
+    ]);
+    sprite.addScript('runRight', [
+      { sprite: 7 },
+      { sprite: 8 },
+      { sprite: 9 },
+      { sprite: 10 }
+    ]);
+    this.set('alpacaSprite', sprite);
+  },
+  didRender() {
     this.$().attr({ tabindex: 1 });
     this.$().focus();
   },
 
   keyDown(event) {
-    let left = this.get('left');
-    let top = this.top;
+    const sprite = this.alpacaSprite;
+    const spriteScript = this.spriteScript;
+    let facingLeft = this.facingLeft;
+    let leftPosition = this.leftPosition;
+    let topPosition = this.topPosition;
+    const currentSprite = sprite.currentSprite();
 
     if (event.keyCode == 68) { // right
-      left = left + 10;
-      this.set('left', left);
-      document.getElementById("moveDiv").style.left = left + "px";
+      leftPosition = leftPosition + 10;
+      this.set('leftPosition', leftPosition);
+
+      if (spriteScript !== 'runRight' || currentSprite === 11) {
+        sprite.play('runRight', {
+          run: -1,
+          delay: 150,
+        });
+      }
+
+      document.getElementById("moveDiv").style.left = leftPosition + "px";
+      this.set('spriteScript', 'runRight');
+      this.set('facingLeft', false);
     }
     if (event.keyCode == 65) { // left
-      left = left - 10;
-      this.set('left', left);
-      document.getElementById("moveDiv").style.left = left + "px";
+      leftPosition = leftPosition - 10;
+      this.set('leftPosition', leftPosition);
+
+      if (spriteScript !== 'runLeft') {
+        sprite.play('runLeft', {
+          run: -1,
+          delay: 150
+        });
+      }
+      document.getElementById("moveDiv").style.left = leftPosition + "px";
+      this.set('spriteScript', 'runLeft');
+      this.set('facingLeft', true);
     }
     if (event.keyCode == 87) { // up
-      top = top - 10;
-      this.set('top', top);
-      document.getElementById("moveDiv").style.top = top + "px";
+      topPosition = topPosition - 10;
+      this.set('topPosition', topPosition);
+      sprite.next();
+
+      if (facingLeft) {
+        sprite.showSprite(2);
+        this.set('spriteScript', 'jumpLeft');
+      } else {
+        sprite.showSprite(11);
+        this.set('spriteScript', 'jumpRight');
+      }
+
+      document.getElementById("moveDiv").style.top = topPosition + "px";
     }
     if (event.keyCode == 83) { // down
-      top = top + 10;
-      this.set('top', top);
-      document.getElementById("moveDiv").style.top = top + "px";
+      topPosition = topPosition + 10;
+      this.set('topPosition', topPosition);
+      
+      document.getElementById("moveDiv").style.top = topPosition + "px";
+    }
+  },
+  keyUp() {
+    const sprite = this.alpacaSprite;
+    const currentSprite = sprite.currentSprite();
+
+    if (currentSprite < 7) {
+      sprite.play({
+        run: 1,
+        script: [
+          { sprite: 3 },
+          { sprite: 1 }]
+      });
+      this.set('spriteScript', 'standLeft');
+    } else {
+      sprite.play({
+        run: 1,
+        script: [{ sprite: 12 }]
+      });
+      this.set('spriteScript', 'standRight');
     }
   }
 });
