65e867cd4b74bb57d51dd184ea75ef77f369dc89
Abbie Glunn <aglunn@gmail.com>
2019-10-15 21:06:36 -0700

    Fix linting errors

--- app/controllers/awesome.js
+++ app/controllers/awesome.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-    typedStrings: ["Ember apps.", "RESTful APIs.", "React components.", "SQL queries.", "automated tests with Selenium."]
+    typedStrings: Object.freeze(["Ember apps.", "RESTful APIs.", "React components.", "SQL queries.", "automated tests with Selenium."])
 });
